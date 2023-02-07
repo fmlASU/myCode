2eb4bb1bd41da06eac76af43a280b128e3c558a7
smuwxia <124534809+smuwxia@users.noreply.github.com>
2023-02-06 23:30:07 -0800

    Pls ignore the change in first line (#3)
    * test2
    
    * test3
    
    * test4
    
    * sdfsdfs

--- ASU_578/project1_2.py
+++ ASU_578/project1_2.py
@@ -1,27 +1,28 @@
-fileName = 'D:\myCode\ASU_578\\adult.data'
+fileName = 'C:\\temp\myCode\ASU_578\\adult.data'
 fileObj = open(fileName)
 
-gender_salary = {'> 50k':{}, '<= 50k': {}}
-
-gender_salary['> 50k']['Male'] = 0
-gender_salary['> 50k']['Female'] = 0
-gender_salary['<= 50k']['Male'] = 0
-gender_salary['<= 50k']['Female'] = 0
+Up50K_Male = 0
+Up50K_Female = 0
+Down50K_Male = 0
+Down50K_Female = 0
 
 for oneLine in fileObj:
-    Items = oneLine[0: len(oneLine) - 1].split(sep = ',')
-    
-    for index in Items:
-        if Items[14] == ' >50k' and Items[9] == ' Male':
-            gender_salary['> 50k']['Male'] += 1
-        elif Items[14] == ' >50k' and Items[9] == ' Female':
-            gender_salary['> 50k']['Female'] += 1
-        elif Items[14] == ' <=50k' and Items[9] == ' Male':
-            gender_salary['<= 50k']['Male'] += 1
-        elif Items[14] == ' <=50k' and Items[9] == ' Female':
-            gender_salary['<= 50k']['Female'] += 1
 
-print(gender_salary)
+    if len(oneLine) < 15:
+        continue
+
+    Items = oneLine[0: len(oneLine) - 1].split(sep = ',')
+
+    if Items[14] == ' >50K' and Items[9] == ' Male':
+        Up50K_Male += 1
+    elif Items[14] == ' >50K' and Items[9] == ' Female':
+        Up50K_Female += 1
+    elif Items[14] == ' <=50K' and Items[9] == ' Male':
+        Down50K_Male += 1
+    elif Items[14] == ' <=50K' and Items[9] == ' Female':
+        Down50K_Female += 1
+
+print(Up50K_Male, Up50K_Female, Down50K_Male, Down50K_Female)
 
 
 fileObj.close()
